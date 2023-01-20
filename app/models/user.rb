c9034c2d45020eed53a0c211f372d5b9842c4ea3
ryujidon <ryujidon110@gmail.com>
2023-01-20 19:05:00 +0900

    パスワードの制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,7 @@ class User < ApplicationRecord
 
          has_many :prototypes
          has_many :comments
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 end

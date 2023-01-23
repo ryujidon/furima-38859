87302a6e1205061ac8a4b060c885133ac42c817c
ryujidon <ryujidon110@gmail.com>
2023-01-23 17:24:32 +0900

    単体テスト実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :first_name, presence: true , format:{ with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'} 
+         validates :last_name, presence: true , format:{ with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'} 
+         validates :first_name_kana, presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: 'はカタカナで入力して下さい。'}
+         validates :last_name_kana, presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: 'はカタカナで入力して下さい。'}
          validates :birthday, presence: true
          
 
@@ -17,4 +17,8 @@ class User < ApplicationRecord
 
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
+
+
+         
 end

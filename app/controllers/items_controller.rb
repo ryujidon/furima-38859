9630916baccff47ced5a4efcb26434ede970ed56
ryujidon <ryujidon110@gmail.com>
2023-01-24 18:09:55 +0900

    投稿機能（コントローラの修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,25 @@
 class ItemsController < ApplicationController
   def index
-    @item = item.all
+    @item = Item.order("created_at DESC")
   end
 
   def new
-    @item = item.new
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to_ root_path
+    else
+      render :new
+    end
   end
 
+  private
 
+  def item_params
+    params.require(:item).permit(:title, :image, :price, :explanation, :category_id ,:item_state_id, :delivery_charge_id, :place_id, :delivery_date_id).merge(user_id: current_user.id)
+  end
 
 end

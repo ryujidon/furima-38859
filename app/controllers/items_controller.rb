b6803733ffbe411ecb47fb711a80bc3fdbdde15f
ryujidon <ryujidon110@gmail.com>
2023-01-31 20:02:51 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update,:destroy]
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
@@ -38,6 +38,13 @@ class ItemsController < ApplicationController
          render :edit
        end
   end  
+
+  def destroy
+    @item.delete
+    redirect_to root_path
+  end
+
+
 
   private
 

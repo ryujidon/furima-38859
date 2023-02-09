d934d6ec307c6fd123d8bfdec3d925253341d788
ryujidon <ryujidon110@gmail.com>
2023-02-09 19:29:37 +0900

    自動デプロイエラー対策

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :application, 'furima-38859'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'https://github.com/ryujidon/furima-38859.git'
 
+set :branch, 'main'
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
